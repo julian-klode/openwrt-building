987b704b246fccb75155e61ab78a1049d3d06f59
Julian Andres Klode <jak@jak-linux.org>
2017-02-04 00:25:10 +0100

    make.sh: Update project path

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Configure the default image for OpenWRT in Studentenwerk
-make image PROFILE=TLWR741 PACKAGES="luci wpad privoxy -wpad-mini" FILES="/home/jak/Projects/Temporary/openwrt/files/"
+make image PROFILE=TLWR741 PACKAGES="luci wpad privoxy -wpad-mini" FILES="/home/jak/Projects/Stable/openwrt/files/"
