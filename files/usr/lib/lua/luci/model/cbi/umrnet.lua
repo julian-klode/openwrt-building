745d8b76b8f8d6bc50b9b3c08c50887bde696db4
Julian Andres Klode <jak@jak-linux.org>
2017-05-21 19:59:42 +0200

    Add quick WiFi configuration to the urment cbi model
    This makes things easier to discover.

--- files/usr/lib/lua/luci/model/cbi/umrnet.lua
+++ files/usr/lib/lua/luci/model/cbi/umrnet.lua
@@ -17,4 +17,14 @@ local s_netdetails = network:section(NamedSection, "wan", "interface", translate
     local o_mac = s_netdetails:option(Value, "macaddr", translate("MAC Address"), translate("The ID of the network adapter previously used / registered with HRZ. Has a form like AB:AB:AB:AB:AB:AB or AB-AB-AB-AB-AB-AB"))
 
     o_mac.datatype = "macaddr"
-return umrnet, network 
+
+-- Wireless setup
+local wireless = Map("wireless")
+
+local s_wireless = wireless:section(NamedSection, "default_radio0", "wifi-iface", translate("WiFi Settings"), translate("Quickly configure your WiFi network name and password. More settings are available in Network&rarr;Wireless."))
+    local o_wireless_ssid = s_wireless:option(Value, "ssid", translate("WiFi network name"))
+    local o_wireless_key = s_wireless:option(Value, "key", translate("WiFi password"))
+    o_wireless_key.password = true
+    o_wireless_key.datatype = "wpakey"
+
+return umrnet, network, wireless
