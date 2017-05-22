59fa396ec47cf9c8f4660debd1e2b7ab9d6bb667
Julian Andres Klode <jak@jak-linux.org>
2017-05-22 10:58:27 +0200

    Enable Smart Queue Management by default
    This improves pings under load considerably. 8000/8000 seems to work
    OK, reducing ping times with parallel downloads or uploads by factor
    10.

--- make.sh
+++ make.sh
@@ -4,4 +4,4 @@
 make image PROFILE=tl-wr741nd-v4 PACKAGES="wpad privoxy -wpad-mini" FILES="/home/jak/Projects/Stable/openwrt/files/"
 
 # Image for the DIR505A1
-make image PROFILE=DIR505A1 PACKAGES="luci wpad -wpad-mini" FILES="/home/jak/Projects/Stable/openwrt/files/"
+make image PROFILE=DIR505A1 PACKAGES="luci luci-app-sqm wpad -wpad-mini" FILES="/home/jak/Projects/Stable/openwrt/files/"
