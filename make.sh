a4c905bab5569e681e444bfaaf4d3050c9ea8503
Julian Andres Klode <jak@jak-linux.org>
2017-05-22 12:53:42 +0200

    make.sh: Also ship SQM on 741

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Configure the default image for OpenWRT in Studentenwerk
-make image PROFILE=tl-wr741nd-v4 PACKAGES="wpad privoxy -wpad-mini" FILES="/home/jak/Projects/Stable/openwrt/files/"
+make image PROFILE=tl-wr741nd-v4 PACKAGES="wpad privoxy sqm-scripts -wpad-mini" FILES="/home/jak/Projects/Stable/openwrt/files/"
 
 # Image for the DIR505A1
 make image PROFILE=DIR505A1 PACKAGES="luci luci-app-sqm wpad -wpad-mini" FILES="/home/jak/Projects/Stable/openwrt/files/"
